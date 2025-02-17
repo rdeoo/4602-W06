0da2eac946e0b320df018d14607d5db1e8697eb3
Rohan Deo <rohandeo307@gmail.com>
2025-02-17 03:13:26 -0800

    finished adding remaining crud functions

--- FifaGamesFunctions.cs
+++ FifaGamesFunctions.cs
@@ -66,4 +66,78 @@ public class FifaGamesFunctions
         response2.WriteStringAsync(JsonConvert.SerializeObject(game));
         return response2;
     }
+
+    [Function("CreateFifaGame")]
+    public HttpResponseData CreateFifaGame
+    (
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "fifagames")] HttpRequestData req
+    )
+    {
+        _logger.LogInformation("C# HTTP POST/posts trigger function processed a request.");
+        var game = JsonConvert.DeserializeObject<Game>(req.ReadAsStringAsync().Result);
+        _context.Games.Add(game);
+        _context.SaveChanges();
+        var response = req.CreateResponse(HttpStatusCode.Created);
+        response.Headers.Add("Content-Type", "application/json");
+        response.WriteStringAsync(JsonConvert.SerializeObject(game));
+        return response;
+    }
+
+    [Function("UpdateFifaGame")]
+    public HttpResponseData UpdateFifaGame
+    (
+        [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "fifagames/{id}")] HttpRequestData req,
+        int id
+    )
+    {
+        _logger.LogInformation("C# HTTP PUT/posts trigger function processed a request.");
+        var game = _context.Games.FindAsync(id).Result;
+        if (game == null)
+        {
+            var response = req.CreateResponse(HttpStatusCode.NotFound);
+            response.Headers.Add("Content-Type", "application/json");
+            response.WriteStringAsync("Not Found");
+            return response;
+        }
+
+        var game2 = JsonConvert.DeserializeObject<Game>(req.ReadAsStringAsync().Result);
+        game.GameId = game2.GameId;
+        game.Year = game2.Year;
+        game.Gender = game2.Gender;
+        game.City = game2.City; 
+        game.Country = game2.Country; 
+        game.Continent = game2.Continent; 
+        game.Winner = game2.Winner; 
+        game.Created = game2.Created; 
+
+        _context.SaveChanges();
+        var response2 = req.CreateResponse(HttpStatusCode.OK);
+        response2.Headers.Add("Content-Type", "application/json");
+        response2.WriteStringAsync(JsonConvert.SerializeObject(game));
+        return response2;
+    }
+
+    [Function("DeleteFifaGame")]
+    public HttpResponseData DeleteFifaGame
+    (
+      [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "fifagames/{id}")] HttpRequestData req,
+      int id
+    )
+    {
+        _logger.LogInformation("C# HTTP DELETE/posts trigger function processed a request.");
+        var game = _context.Games.FindAsync(id).Result;
+        if (game == null)
+        {
+            var response = req.CreateResponse(HttpStatusCode.NotFound);
+            response.Headers.Add("Content-Type", "application/json");
+            response.WriteStringAsync("Not Found");
+            return response;
+        }
+        _context.Games.Remove(game);
+        _context.SaveChanges();
+        var response2 = req.CreateResponse(HttpStatusCode.OK);
+        response2.Headers.Add("Content-Type", "application/json");
+        response2.WriteStringAsync(JsonConvert.SerializeObject(game));
+        return response2;
+    }
 }
